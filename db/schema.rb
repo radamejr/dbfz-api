f80dd21fcb0dc9b8dc49d6b2c61f84b2d910089a
radamejr <radamejr@gmail.com>
2019-11-14 16:44:35 +0000

    Added normals for characters

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_154636) do
+ActiveRecord::Schema.define(version: 2019_11_14_162252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_11_14_154636) do
     t.string "immune_to"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "character_id"
   end
 
 end

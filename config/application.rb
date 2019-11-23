7aa23ef26acdb1017582403554b9170cbf5fc9c9
radamejr <50850876+radamejr@users.noreply.github.com>
2019-11-22 19:32:59 -0700

    updated rack

--- config/application.rb
+++ config/application.rb
@@ -21,13 +21,15 @@ module DbfzChar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.middleware.use ActionDispatch::Flash
+
     config.middleware.use Rack::Cors do
       allow do
         origins '*'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete, :options]
       end
     end
+    config.middleware.use ActionDispatch::Flash
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

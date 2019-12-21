8f5c95eea956747b09384208d19904ba38bc88d8
Robert Adame <radamejr@gmail.com>
2019-12-21 09:27:27 -0700

    updated session store.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'https://dbfztech-api.herokuapp.com/'
+    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'http://localhost:8080/'
   else
     Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz' 
   end

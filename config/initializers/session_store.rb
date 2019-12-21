688c57584820df47eb5119f5615c47fb213eb645
Robert Adame <radamejr@gmail.com>
2019-12-21 10:00:24 -0700

    added samesite configuration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'http://localhost:8080/', expire_after: 14.days
+    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'http://localhost:8080/', expire_after: 14.days, same_site: :lax
   else
     Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz' 
 end

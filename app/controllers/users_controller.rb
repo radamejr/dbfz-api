587d1ebff6c96f47f062318ce039bc2e68da2542
Robert Adame <radamejr@gmail.com>
2019-12-21 07:25:39 -0700

    further updates for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation, :admin)
     end
 end

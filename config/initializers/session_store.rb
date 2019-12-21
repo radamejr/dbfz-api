809a0edd711f3510ef0cfc3e65fbf2f1913529b1
Robert Adame <radamejr@gmail.com>
2019-12-21 06:45:27 -0700

    adjusted for heroku push

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_dbfz', domain: 'your-frontend-domain'
+    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'https://dbfztech-api.herokuapp.com/'
   else
     Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz' 
   end

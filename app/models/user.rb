79647f4bd922b32f46e2e0c4587785a599e8f68c
Robert Adame <radamejr@gmail.com>
2019-12-21 07:27:54 -0700

    added validation for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
     
     validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-    #validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }
 
 end

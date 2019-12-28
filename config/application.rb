3300554f8f6418a883d17351e9fd3afdf42995c5
Robert Adame <radamejr@gmail.com>
2019-12-28 10:33:19 -0700

    changed for only one origin

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module DbfzChar
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins ['http://localhost:8080', 'https://framehameha.herokuapp.com/']
+        origins 'https://framehameha.herokuapp.com/'
         
         resource '*',
         headers: :any,

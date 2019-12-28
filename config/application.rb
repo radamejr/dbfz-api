a9ef0cce0771a1ddf1e6201884ee8980beda8baf
Robert Adame <radamejr@gmail.com>
2019-12-28 09:20:56 -0700

    added front end.

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module DbfzChar
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:8080', 'http://localhost:3030'
+        origins 'http://localhost:8080', 'https://framehameha.herokuapp.com/'
         
         resource '*',
         headers: :any,

bd8eee93693720627e12d0c457e02443aa425501
radamejr <50850876+radamejr@users.noreply.github.com>
2020-03-11 15:41:56 -0700

    added samesite

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'https://www.framehameha.com/', expire_after: 14.days, secure: true, same_site: :none
+    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'https://www.framehameha.com/', expire_after: 14.days
   else
-    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', secure: true, same_site: :none
+    Rails.application.config.session_store :cookie_store, key: '_Dragonball Fighterz', domain: 'http://localhost/'
 end

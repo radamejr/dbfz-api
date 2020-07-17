27f8d0d114a835e1473050d30733c97e63b6d555
radamejr <50850876+radamejr@users.noreply.github.com>
2020-07-16 19:54:06 -0700

    setup dev variables

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,5 @@
-CarrierWave.configure do |config|
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'                        # required
     config.fog_public = false
     config.fog_credentials = {
@@ -8,3 +9,17 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = ENV["AWS_BUCKET"]              # required
   end
+end
+
+if Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'                        # required
+    config.fog_public = false
+    config.fog_credentials = {
+      provider:              'AWS',                        # required
+      aws_access_key_id:     ENV["DEV_AWS_ACCESS_KEY"],        # required
+      aws_secret_access_key: ENV["DEV_AWS_SECRET_KEY"],        # required
+    }
+    config.fog_directory  = ENV["DEV_AWS_BUCKET"]              # required
+  end
+end
